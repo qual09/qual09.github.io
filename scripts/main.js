66b0b201a6ecddc22027033ed298bb32a416f36b
Qual09 <61503285+qual09@users.noreply.github.com>
2020-03-11 15:14:08 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,20 @@
 function openHome(){
 	$('#mainContent').load('home.html');
+	document.getElementById("home").classList.add('currentMenu');
+	document.getElementById("blog").classList.remove('currentMenu');
+	document.getElementById("about").classList.remove('currentMenu');
 }
 
 function openBlog(){
 	$('#mainContent').load('blog.html');
+	document.getElementById("blog").classList.add('currentMenu');
+	document.getElementById("home").classList.remove('currentMenu');
+	document.getElementById("about").classList.remove('currentMenu');
 }
 
 function openAbout(){
 	$('#mainContent').load('about.html');
+	document.getElementById("about").classList.add('currentMenu');
+	document.getElementById("home").classList.remove('currentMenu');
+	document.getElementById("blog").classList.remove('currentMenu');
 }
