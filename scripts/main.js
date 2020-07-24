3a2a81bbddd0730d20ca8b447fb7ed5353ea0767
HCPX <cpx.hieu@gmail.com>
2020-07-24 20:21:58 +0200

    links update

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,10 @@ function yo() {
 	alert("Yo Dawg!");
 }
 
-function openHome() {
-	$('#mainContent').load('home.html');
-	document.getElementById("home").classList.add('currentMenu');
-	document.getElementById("blog").classList.remove('currentMenu');
-	document.getElementById("about").classList.remove('currentMenu');
-}
-
-function openBlog() {
-	$('#mainContent').load('blog.html');
-	document.getElementById("home").classList.remove('currentMenu');
-	document.getElementById("blog").classList.add('currentMenu');
-	document.getElementById("about").classList.remove('currentMenu');
-}
-
-function openAbout() {
-	$('#mainContent').load('about.html');
+function openContent(content) {
+	$('#mainContent').load(content + '.html');
 	document.getElementById("home").classList.remove('currentMenu');
 	document.getElementById("blog").classList.remove('currentMenu');
-	document.getElementById("about").classList.add('currentMenu');
+	document.getElementById("about").classList.remove('currentMenu');
+	document.getElementById(content).classList.add('currentMenu');
 }
