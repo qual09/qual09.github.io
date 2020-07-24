07ff29f9e1d9340f60e5943087bbcd14d40a505a
HCPX <cpx.hieu@gmail.com>
2020-07-24 20:30:17 +0200

    fix currentMenu

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,10 @@ function yo() {
 
 function openContent(content) {
 	$('#mainContent').load(content + '.html');
+	selectCurrentNav(content);
+}
+
+function selectCurrentNav(content) {
 	document.getElementById("home").classList.remove('currentMenu');
 	document.getElementById("blog").classList.remove('currentMenu');
 	document.getElementById("about").classList.remove('currentMenu');
