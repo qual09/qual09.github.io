a53d591d621544e23ccaa131f7770e6c8c99fd74
HCPX <cpx.hieu@gmail.com>
2020-07-24 22:40:39 +0200

    sticky header added

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,15 @@ function selectCurrentNav(content) {
 	document.getElementById("about").classList.remove('currentMenu');
 	document.getElementById(content).classList.add('currentMenu');
 }
+
+//STICKY HEADER
+window.onscroll = () => stickyHeader();
+function stickyHeader() {
+	var header = document.getElementById("stickyHeader");
+	var sticky = header.offsetTop;
+	if (window.pageYOffset > sticky) {
+		header.classList.add("stickyHeader");
+	} else {
+		header.classList.remove("stickyHeader");
+	}
+}
