4fb54a5f28ed05da1db81fbe07c4a206cb3c4c8e
Hieu CPX <hieu.chu@lingarogroup.com>
2020-08-13 16:40:32 +0200

    ok

---
+++ portfolio/book-list/app.js
@@ -0,0 +1,37 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {
+
+  UI.prototype.addBookToList = function (book) {
+    console.log(book);
+  }
+}
+
+
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  // Get form values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  console.log(ui);
+
+  e.preventDefault();
+});
