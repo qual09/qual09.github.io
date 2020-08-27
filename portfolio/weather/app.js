d8a382493d8e56053f0d96ecba542374e4fcec3f
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-08-27 17:06:19 +0200

    change location local storage

--- portfolio/weather/app.js
+++ portfolio/weather/app.js
@@ -1,11 +1,32 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init Weather object
-const weather = new Weather('Warsaw', 'PL');
-
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 // Init UI object
 const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  // Change location
+  weather.changeLocation(city, country);
+
+  // Set location in LS
+  storage.setLocationData(city, country);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+});
 
 // weather.changeLocation('Miami', 'US');
 // weather.changeLocation('London', 'GB');
