f22e52d6d7294e59481001451bb4f91ebae28594
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-08-27 12:27:37 +0200

    fetch data

--- portfolio/github-finder/app.js
+++ portfolio/github-finder/app.js
@@ -1,1 +1,26 @@
+// Init github
+const github = new GitHub;
+
 // Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== '') {
+    // Make http call
+    github.getUser(userText)
+      .then(data => {
+        console.log(data);
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+
+        } else {
+          // Show profile
+
+        }
+      })
+  }
+});
--- portfolio/github-finder/github.js
+++ portfolio/github-finder/github.js
@@ -1,6 +1,19 @@
 // https://github.com/settings/applications/1363981
-// Client ID
-// e2c2f9d1e052d1d24dd0
-// Client Secret
-// 64536bb35baa7c37934b894e12f3193ddf7b6430
 
+class GitHub {
+  constructor() {
+    this.client_id = 'e2c2f9d1e052d1d24dd0';
+    this.client_secret = '64536bb35baa7c37934b894e12f3193ddf7b6430';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile //same as profile: profile
+    }
+  }
+
+}
