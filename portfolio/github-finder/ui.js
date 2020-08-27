7e31b31663a9b7b346bc756ce9fc7d84796aacae
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-08-27 14:30:02 +0200

    ui and date upd

--- portfolio/github-finder/ui.js
+++ portfolio/github-finder/ui.js
@@ -9,21 +9,21 @@ class UI {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
         <div class="row">
-          <div class="col-md-3 mb-3">
+          <div class="col-md-3 mb-2">
             <img class="img-fluid mb-2" src="${user.avatar_url}">
             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
           </div>
           <div class="col-md-9">
-            <span class="badge badge-primary mb-2">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-secondary mb-2">Public Gists: ${user.public_gists}</span>
-            <span class="badge badge-success mb-2">Followers: ${user.followers}</span>
-            <span class="badge badge-info mb-2">Following: ${user.following}</span>
+            <span class="badge badge-primary mb-1">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary mb-1">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success mb-1">Followers: ${user.followers}</span>
+            <span class="badge badge-info mb-1">Following: ${user.following}</span>
             <br>
             <ul class="list-group mt-1">
               <li class="list-group-item">Company: ${user.company}</li>
               <li class="list-group-item">Website/Blog: <a href="${user.blog}" target="_blank">${user.blog}</a></li>
               <li class="list-group-item">Location: ${user.location}</li>
-              <li class="list-group-item">Member Since: ${user.created_at}</li>
+              <li class="list-group-item">Member Since: ${user.created_at.substring(0, 10)}</li>
             </ul>
           </div>
         </div>      
@@ -46,7 +46,7 @@ class UI {
       output += `
         <div class="card card-body mb-2">
           <div clas="row">
-            <div class="col-md-6">
+            <div class="col-md-6 mb-1">
               <a href="${repo.html_url}" target="_blank">${repo.name}</a>
             </div>
             <div class="col-md-6">
