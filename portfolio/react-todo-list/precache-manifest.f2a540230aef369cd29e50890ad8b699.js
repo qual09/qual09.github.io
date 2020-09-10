693f0f27c3cd34dc0509a586421475ab2ba13699
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-09-10 16:13:52 +0200

    deployed react todolist

---
+++ portfolio/react-todo-list/precache-manifest.f2a540230aef369cd29e50890ad8b699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5fd3fd143df308e2f0e371052468da",
+    "url": "/portfolio/react-todo-list/index.html"
+  },
+  {
+    "revision": "d7d5c67bc4e18759e3db",
+    "url": "/portfolio/react-todo-list/static/css/main.43f1aaee.chunk.css"
+  },
+  {
+    "revision": "52cba017b9d25ba3ccb7",
+    "url": "/portfolio/react-todo-list/static/js/2.cbb7c2d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/react-todo-list/static/js/2.cbb7c2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d5c67bc4e18759e3db",
+    "url": "/portfolio/react-todo-list/static/js/main.d4ff1c45.chunk.js"
+  },
+  {
+    "revision": "aede2699ccaaab951790",
+    "url": "/portfolio/react-todo-list/static/js/runtime-main.4a18c0aa.js"
+  }
+]);
