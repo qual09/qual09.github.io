7b50ed40d7ac1f253cd2e55ac4cb1f8f7f322c15
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-09-23 18:18:23 +0200

    ui update and cleanup

--- portfolio/beata-patrzalek/scripts/main.js
+++ portfolio/beata-patrzalek/scripts/main.js
@@ -1,18 +1,38 @@
-//STICKY HEADER
+// Sticky Header
 window.onscroll = function () { stickyHeader() };
 function stickyHeader() {
-	var header = document.getElementById("stickyHeader");
-	var sticky = header.offsetTop;
-
+	let header = document.getElementById('stickyHeader');
+	let sticky = header.offsetTop;
 	if (window.pageYOffset > sticky) {
-		header.classList.add("stickyHeader");
+		header.classList.add('stickyHeader');
 	} else {
-		header.classList.remove("stickyHeader");
+		header.classList.remove('stickyHeader');
 	}
 }
 
 // Slider
-// Code By Webdevtrick ( https://webdevtrick.com )
 function beforeAfter() {
-	document.getElementById('compare').style.width = document.getElementById('slider').value + "%";
+	document.getElementById('compare').style.width = document.getElementById('slider').value + '%';
+}
+
+// Load Dynamic Content
+function openContent(content) {
+	$('#dynamiContent').load(content + '.html');
+	selectCurrentNav(content);
+
+	document.querySelector('.slideshow').style.display = 'none';
+	document.querySelector('.title').style.display = 'none';
+	document.querySelector('#dynamiContent').style.display = 'block';
+}
+
+// Set current navigation in menu
+function selectCurrentNav(content) {
+	const ul = document.getElementById('navMenu');
+	Array.from(ul.children).forEach(li => {
+		if (li.children[0].id === content) {
+			li.children[0].classList.add('currentMenu');
+		} else {
+			li.children[0].classList.remove('currentMenu');
+		}
+	});
 }
--- portfolio/beata-patrzalek/scripts/slideshow.js
+++ portfolio/beata-patrzalek/scripts/slideshow.js
@@ -1,4 +1,8 @@
-//SLIDESHOW
+let slideIndex = 0;
+let carouselTimeout;
+//showDivs(slideIndex);
+carousel();
+
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
@@ -8,36 +12,36 @@ function currentDiv(n) {
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("bgDot");
+  let i;
+  let x = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('bgDot');
   if (n > x.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" bgDot-white", "");
+    dots[i].className = dots[i].className.replace(' bgDot-white', '');
   }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " bgDot-white";
+  x[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' bgDot-white';
   clearTimeout(carouselTimeout);
   carouselTimeout = setTimeout(carousel, 9000);
 }
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("bgDot");
+  let i;
+  let x = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('bgDot');
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" bgDot-white", "");
+    dots[i].className = dots[i].className.replace(' bgDot-white', '');
   }
   slideIndex++;
   if (slideIndex > x.length) { slideIndex = 1 }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " bgDot-white";
+  x[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' bgDot-white';
   carouselTimeout = setTimeout(carousel, 5000); // Change image every 2 seconds (2000)
 }
