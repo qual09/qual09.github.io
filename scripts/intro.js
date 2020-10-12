98d598089b284ce8d2b3521afe6777bff69858bc
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-10-12 15:36:31 +0200

    no slider

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,10 +1,11 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.to(".intro", { y: "-100%", duration: 1, delay: 1.5 }, "-=1");
 
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.5 });
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.5 });
 
 setTimeout(() => {
   document.querySelector('.intro').remove();
