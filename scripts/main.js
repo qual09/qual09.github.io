3faf6f69d9998eb11389dab49c9be7d204afd32a
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-11-13 10:55:08 +0100

    test function

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ openContent('home');
 // *** START TEST *** //
 console.log('Yo Dawg!');
 console.log('Welcome to', location.hostname);
-// TEST function
+// TEST function 1
 function yo() {
   const searchCriteria = {
     name: 'John',
@@ -21,6 +21,27 @@ function yo() {
     // if (searchCriteria.hasOwnProperty(prop))
     console.log(prop, searchCriteria[prop]);
   };
+}
+// TEST function 2
+function yo2() {
+  const searchCriteria = [{ key: "AAA", value: ['a', 'b', 'c'] }, { key: "BBB", value: ['x', 'y', 'z'] }];
+  console.log(searchCriteria);
+  let newSearchCriteria = [];
+  searchCriteria.forEach(element => {
+    let tempKey;
+    let tempValue;
+    for (let prop in element) {
+      // console.log(prop, element[prop]);
+      if (prop === 'key') {
+        tempKey = element[prop];
+      }
+      if (prop === 'value') {
+        tempValue = element[prop];
+        newSearchCriteria.push({ [tempKey]: tempValue });
+      }
+    };
+  });
+  console.log(newSearchCriteria);
 }
 // *** END TEST *** //
 
