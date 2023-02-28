66ffb8dce72da2666dfa4cad83d634b245229c8e
Qual09 <cpx.hieu@gmail.com>
2023-02-28 22:59:55 +0100

    animation and style update

--- scripts/intro.js
+++ scripts/intro.js
@@ -6,16 +6,16 @@ const timeline = gsap.timeline({ defaults: { ease: "power1.out" } });
 timeline.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 // timeline.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 // timeline.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-timeline.to(".intro", { y: "-100%", duration: 1, delay: 1.5 }, "-=1");
+timeline.to(".intro", { y: "-100%", duration: 2, delay: 2 }, "-=1");
 
 // timeline.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.5 });
 
 setTimeout(() => {
   mainWrapper.style.display = 'block';
-}, 2600);
+}, 3000);
 
 setTimeout(() => {
   document.querySelector('.intro').remove();
   document.querySelector('.slider').remove();
-}, 3000);
+}, 5000);
 
